c73607d9864a204081b9ccefd99e2dd2e1fa2e48
dinggd <gd.ding.cs@gmail.com>
2024-04-05 16:01:01 +0800

    c2f placeholder

---
+++ model/c2f.py
@@ -0,0 +1,12 @@
+from model.base import BaseTrainer
+
+
+class C2FModel:
+    def __init__(self) -> None:
+        pass
+
+
+class C2FTrainer(BaseTrainer):
+
+    def __init__(self, cfg):
+        self.model = C2FModel()
